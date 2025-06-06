84b9c207e75750aaee452d6e60be7409697ccab6
Fredrik Bergström <basn@lan2k.org>
2025-06-06 09:42:51 +0200

    testing powersettings

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -13,57 +13,61 @@
   networking.hostName = "laptop"; 
   networking.networkmanager.enable = true;
   time.timeZone = "Europe/Stockholm";
-  i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sv_SE.UTF-8";
-    LC_IDENTIFICATION = "sv_SE.UTF-8";
-    LC_MEASUREMENT = "sv_SE.UTF-8";
-    LC_MONETARY = "sv_SE.UTF-8";
-    LC_NAME = "sv_SE.UTF-8";
-    LC_NUMERIC = "sv_SE.UTF-8";
-    LC_PAPER = "sv_SE.UTF-8";
-    LC_TELEPHONE = "sv_SE.UTF-8";
-    LC_TIME = "sv_SE.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "sv_SE.UTF-8";
+      LC_IDENTIFICATION = "sv_SE.UTF-8";
+      LC_MEASUREMENT = "sv_SE.UTF-8";
+      LC_MONETARY = "sv_SE.UTF-8";
+      LC_NAME = "sv_SE.UTF-8";
+      LC_NUMERIC = "sv_SE.UTF-8";
+      LC_PAPER = "sv_SE.UTF-8";
+      LC_TELEPHONE = "sv_SE.UTF-8";
+      LC_TIME = "sv_SE.UTF-8";
+    };
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.lightdm.enable = true;
+      desktopManager.pantheon.enable = true;
+    };
+    auto-cpufreq = {
+      enable = true;
+      battery = {
+        govenor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        govenor = "performance";
+        turbo = "auto";
+      };
+    };
+  };
 
-  # Enable the Pantheon Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.pantheon.enable = true;
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "se";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "sv-latin1";
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
+    enable = true;https://github.com/basn/nixos.git
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
   users.users.basn = {
     isNormalUser = true;
     description = "Fredrik Bergström";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
     ];
   };
 
@@ -78,6 +82,9 @@
   fonts.packages = with pkgs; [
     meslo-lgs-nf
   ];
+  powerManagement = {
+    enable = true;
+  };
   system.stateVersion = "25.05";
 
 }
