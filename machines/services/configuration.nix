5056878505ca58be2a831627cafac3aab2a14f9d
Fredrik Bergström <basn@lan2k.org>
2025-06-20 12:50:11 +0200

    added cachix

--- machines/services/configuration.nix
+++ machines/services/configuration.nix
@@ -15,7 +15,7 @@
       ./services/unifi.nix
       ./services/ac.nix
       ./services/teslamate.nix
-      #./services/authentik.nix
+      ./services/authentik.nix
     ];
   boot = {
     zfs = {
@@ -88,8 +88,11 @@
   };
   nix = {
     settings = {
-      trusted-substituters = [
-
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
