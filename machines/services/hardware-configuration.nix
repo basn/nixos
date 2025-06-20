b4c44a30443d5a9bd261f4fdc5bc233e0f5529a6
Fredrik Bergström <basn@lan2k.org>
2025-06-20 13:05:00 +0200

    removed faulty config

--- machines/services/hardware-configuration.nix
+++ machines/services/hardware-configuration.nix
@@ -35,8 +35,5 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
- networking.useDHCP = lib.mkDefault false;
- networking.interfaces.ens33.useDHCP = lib.mkDefault false;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
