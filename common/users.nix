e7d7bc31fbbcd215f41481c548c28fdbcb5b5f0c
Fredrik Bergström <basn@lan2k.org>
2025-06-25 11:42:59 +0200

    added laptop to ssh config

--- common/users.nix
+++ common/users.nix
@@ -10,6 +10,7 @@
             keys = [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdxg/zlW4GiU6kRJcTQ6UYEbF07uBJWD5zhcm6gk//T basn@battlestation"
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJaybi1pQnJp1dE/LhhPGiy94000SpKG6q3Qg0UjryOi evl755@KX4H4MFHW1"
+              "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAFuVdK5MvjlkAypdfCHVqkmNGjK/+8zyq+XXmZ3D/JUtSdI5owSSefXkYhX1QoopQ2HJBsoyzorMIZD9UNixGgQpwHyW7RZ/w2pERcYA5P63eWWvASQ9seO1Bl1ngwztnCs0Xe7KUbdDYF9vyq0liQeelE0eU8ZMLyu9C8nIJN+S9zwog== basn@laptop"
             ];
           };
         };
