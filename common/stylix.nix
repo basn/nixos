6c29bb5b5f4d7e14a554dc36d4f8ebe91852b402
Fredrik Bergström <basn@lan2k.org>
2025-06-28 17:42:54 +0200

    test if hyprland is on use stylix

--- common/stylix.nix
+++ common/stylix.nix
@@ -1,7 +1,7 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
   stylix = {
-    enable = true;
+    enable = config.programs.hyprland.enable;
     base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/onedark.yaml";
     polarity = "dark";
   };
