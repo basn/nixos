1a80daa0c952c01590390f3853a29205c8c40cde
Fredrik Bergström <basn@lan2k.org>
2025-06-29 21:23:47 +0200

    fixed so one window gets fullscreen

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -46,6 +46,16 @@
               enabled = false;
             };
           };
+          workspace = [
+            "w[tv1], gapsout:0, gapsin:0"
+            "f[1], gapsout:0, gapsin:0"
+          ];
+          windowrule = [
+            "bordersize 0, floating:0, onworkspace:w[tv1]"
+            "rounding 0, floating:0, onworkspace:w[tv1]"
+            "bordersize 0, floating:0, onworkspace:f[1]"
+            "rounding 0, floating:0, onworkspace:f[1]"
+          ];
           animations = {
             enabled = true;
             bezier = [
