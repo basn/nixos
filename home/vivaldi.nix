c1bc22ff68a7251caea16d83570627cee27fc25b
Fredrik Bergström <basn@lan2k.org>
2025-07-13 15:18:57 +0200

    forgot to update some files

--- home/vivaldi.nix
+++ home/vivaldi.nix
@@ -6,7 +6,7 @@
       genericName = "Web Browser";
       exec = "vivaldi --ozone-platform=wayland --enable-features=UseOzonePlatform --use-cmd-decoder=validating --use-gl=desktop %U";
       terminal = false;
-      categories = [ "Application" "Network" "WebBrowser" ];
+      categories = [ "Network" "WebBrowser" ];
       mimeType = [ "text/html" "text/xml" ];
     };
   };
