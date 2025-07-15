5d92159d3791c5e67baed9aad3f035cce3b423f4
Fredrik Bergström <basn@lan2k.org>
2025-07-15 19:54:56 +0200

    updated some configs

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -456,7 +456,7 @@
       settings = {
         general = {
           after_sleep_cmd = "hyprctl dispatch dpms on";
-          ignore_dbus_inhibit = false;
+          ignore_dbus_inhibit = true;
           lock_cmd = "pidof hyprlock || hyprlock";
           };
         listener = [
