28ccc9d6762241092db5cdab5bee5022575246c5
Fredrik Bergström <basn@lan2k.org>
2025-08-14 23:01:49 +0200

    updated plex

--- machines/bandit/services/plex.nix
+++ machines/bandit/services/plex.nix
@@ -1,10 +1,22 @@
-{ unstablePkgs, ... }:
+{ pkgs, ... }:
 
 {
-  services.plex = {
-      enable = true;
-      openFirewall = true;
-      package = unstablePkgs.plex;
+  services.plex = let
+  plexPass = pkgs.plex.override {
+    plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
+      version = "1.42.1.10060-4e8b05daf";  # replace with your Plex Pass version
+      src = pkgs.fetchurl {
+        url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
+        sha256 = "3a822dbc6d08a6050a959d099b30dcd96a8cb7266b94d085ecc0a750aa8197f4";
+      };
+    });
   };
+in {
+  enable = true;
+  openFirewall = true;
+  user = "plex";         # optional, your setup
+  group = "plex";        # optional
+  package = plexPass;
+ };
   users.users.plex.extraGroups = [ "video" ];
 }
