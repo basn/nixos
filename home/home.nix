e18753fbb10fa7b78e84bebab6c62c98b84159b6
Fredrik Bergström <basn@lan2k.org>
2025-08-23 10:29:21 +0200

    updated

--- home/home.nix
+++ home/home.nix
@@ -13,17 +13,6 @@
     ssh = {
       addKeysToAgent = "yes";
     };
-    zsh = {
-      enable = false;
-      autosuggestion = {
-        enable = true;
-      };
-      enableCompletion = true;
-      history = {
-        size = 10000;
-	share = true;
-      };
-    };
     fish = {
       enable = true;
     };
@@ -37,11 +26,18 @@
       userName = "Fredrik Bergström";
       userEmail = "basn@lan2k.org";
     };
-    oh-my-posh = {
-      enable = false;
-      package = pkgs.oh-my-posh;
-      enableZshIntegration = true;
-      useTheme = "blue-owl";
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+        command_timeout = 1300;
+        scan_timeout = 50;
+        format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
+        character = {
+          success_symbol = "[](bold green) ";
+          error_symbol = "[✗](bold red) ";
+        };
+      };
     };
     nvf = {
       enable = true;
