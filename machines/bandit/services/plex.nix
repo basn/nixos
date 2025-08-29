226e5e5d4fb3a5dff400bd695bedf01e4bb51c97
Fredrik Bergström <basn@lan2k.org>
2025-08-29 15:10:42 +0200

    update plex pass version

--- machines/bandit/services/plex.nix
+++ machines/bandit/services/plex.nix
@@ -4,7 +4,7 @@
   services.plex = let
   plexPass = pkgs.plex.override {
     plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
-      version = "1.42.1.10060-4e8b05daf";  # replace with your Plex Pass version
+      version = "v.1.42.2.10102-50deb00d9";  # replace with your Plex Pass version
       src = pkgs.fetchurl {
         url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
         sha256 = "3a822dbc6d08a6050a959d099b30dcd96a8cb7266b94d085ecc0a750aa8197f4";
