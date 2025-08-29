6d70d90837392182861b73eab1c6067854d16842
Fredrik Bergström <basn@lan2k.org>
2025-08-29 15:14:44 +0200

    fixed plex

--- machines/bandit/services/plex.nix
+++ machines/bandit/services/plex.nix
@@ -4,10 +4,10 @@
   services.plex = let
   plexPass = pkgs.plex.override {
     plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
-      version = "v.1.42.2.10102-50deb00d9";  # replace with your Plex Pass version
+      version = "1.42.2.10102-50deb00d9";  # replace with your Plex Pass version
       src = pkgs.fetchurl {
         url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-        sha256 = "3a822dbc6d08a6050a959d099b30dcd96a8cb7266b94d085ecc0a750aa8197f4";
+        sha256 = "YR8ir/2SGTqJE+HXBKeG5LqsE1JZnkdjtKSdtFBNVOY=";
       };
     });
   };
