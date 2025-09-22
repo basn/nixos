ca7db5246e2fb424a28bd77610bdcd6462b345bd
Fredrik Bergström <basn@lan2k.org>
2025-09-22 22:28:27 +0200

    changed shell

--- machines/vault/services/rsync.nix
+++ machines/vault/services/rsync.nix
@@ -7,6 +7,7 @@
   };
   users.users.backup = {
     isNormalUser = true;
+    shell = pkgs.bash;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwMs+6QCPJafLNgEjKm1Klq5xPZ8kOHaMdOCyqu7Ddr"
     ];
