6e9abff9c88f26b82894e4886a49aad1093bcb0e
Fredrik Bergström <basn@lan2k.org>
2025-10-22 07:17:01 +0200

    added ghostty config

--- home/gui.nix
+++ home/gui.nix
@@ -5,7 +5,15 @@
  ];
  programs = {
    ghostty = {
-     enable = true; 
+     enable = true;
+      settings = {
+        font-family = "MesloLGMDZ Nerd Font";
+        font-family-bold = "MesloLGMDZ Nerd Font";
+        font-size = 12;
+        theme = "iTerm2 Dark Background";
+        shell-integration = "fish";
+        shell-integration-features = "sudo";
+      };
    };
  };
  home = {
