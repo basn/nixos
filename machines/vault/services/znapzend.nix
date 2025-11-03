3d30d54460d99ec5d51e9219ff3267ed2c51a350
Fredrik Bergström <basn@lan2k.org>
2025-11-03 20:28:24 +0100

    updated znapzend

--- machines/vault/services/znapzend.nix
+++ machines/vault/services/znapzend.nix
@@ -9,10 +9,24 @@
         recursive = true;
         mbuffer.enable = false;
         plan = "1d=>4h,1w=>1d";
-        timestampFormat = "%Y%m%d%H%M%SZ";
+        timestampFormat = "%Y-%m-%d-%H%M%S";
         destinations = {
           "nixos-sov" = {
             dataset = "backup/nas";
+            plan = "1w=>1d";
+            host = "syncoid@nixos-sov";
+          };
+        };
+      };
+      "storage/berget" = {
+        enable = true;
+        recursive = true;
+        mbuffer.enable = false;
+        plan = "1d=>4h,1w=>1d";
+        timestampFormat = "%Y-%m-%d-%H%M%S";
+        destinations = {
+          "nixos-sov" = {
+            dataset = "backup/berget";
             plan = "1w=>1d";
             host = "syncoid@nixos-sov";
           };
