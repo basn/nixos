8908f44e1f86b007a1bad13cb5823cdb839cb5a3
Fredrik Bergström <basn@lan2k.org>
2025-11-05 19:16:25 +0100

    fixes

--- machines/vault/services/backupuser.nix
+++ machines/vault/services/backupuser.nix
@@ -7,10 +7,12 @@
       shell = pkgs.bash;
       openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAAqWEcRiqHQjTc51d54Ws46uNpo0JYc3BBFiWVdTOpxRYGL7mFZNJ8is8srJYpI0MIWNADF2xicgZ2VfYMiQ0S2MwAc7ax+HTx+/RXQEPbPJ5xelJlM1+W8k4Uh4QDvKW9kIGiMeDgtlfgCZQxekX1AmBxSOQuIyRMq0JqFCJ54AO372Q== root@services" ];
       packages = [ pkgs.mbuffer pkgs.lzop ];
+      group = "zfsbackup";
     };
+    groups.zfsbackup = {};
   };
   systemd.services.zfs-permissions = {
-    description = "Grant ZFS permissions to syncoid";
+    description = "Grant ZFS permissions to zfsbackup";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
