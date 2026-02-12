0893c978715d88e4d0db5db463284ffc7a13c1a7
Fredrik Bergström <basn@lan2k.org>
2026-02-12 07:45:42 +0100

    removed home-manager configs since i dont use it anymore

--- home/hyprland/default.nix
+++
@@ -1,5 +0,0 @@
-{ config, ... }:
-{
-  imports = [ ./waybar.nix ./hyprland.nix ./hypridle.nix ./hyprpaper.nix ./rofi.nix ./dunst.nix ./hyprlock.nix ];
-  xdg.configFile."uwsm/env".source = "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh";
-}
