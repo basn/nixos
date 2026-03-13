3a3f3b3cbe4c44d3d655a3d595835c8e73fb4144
Fredrik Bergstrom <basn@lan2k.org>
2026-03-13 12:36:29 +0100

    updated lock

--- common/netbird.nix
+++ common/netbird.nix
@@ -1,5 +1,18 @@
 { ... }:
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      netbird-ui = prev.netbird-ui.overrideAttrs (old: {
+        postInstall =
+          (old.postInstall or "")
+          + ''
+            substituteInPlace "$out/share/applications/netbird.desktop" \
+              --replace-fail 'Exec=netbird-ui' "Exec=$out/bin/netbird-ui"
+          '';
+      });
+    })
+  ];
+
   services = {
     netbird = {
       enable = true;
