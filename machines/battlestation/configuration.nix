de9be97e8ad927d74b84d5c8d258d0eedbd4d905
Fredrik Bergstrom <basn@lan2k.org>
2026-03-30 11:11:23 +0200

    updated flake lock

--- machines/battlestation/configuration.nix
+++ machines/battlestation/configuration.nix
@@ -341,4 +341,10 @@ in
     };
     stateVersion = "25.05";
   };
+  nix = {
+    settings = {
+      substituters = [ "https://attic.xuyh0120.win/lantian" ];
+      trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+    };
+  };
 }
