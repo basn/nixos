062196a31a31ba87b8ead4f8b1e4df55728a0f83
Fredrik Bergstrom <basn@lan2k.org>
2026-04-01 15:01:59 +0200

    fixed noctalia to hjem

--- hjem/noctalia.nix
+++ hjem/noctalia.nix
@@ -1,8 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   hjem = {
     users = {
       basn = {
+        packages = with pkgs; [
+          noctalia-shell
+        ];
         files = {
           ".config/noctalia/settings.json".source = ./configs/noctalia-settings.json;
         };
--- machines/battlestation/configuration.nix
+++ machines/battlestation/configuration.nix
@@ -265,7 +265,6 @@ in
       grim
       slurp
       wl-clipboard
-      noctalia-shell
       rocmPackages.rocm-smi
       rocmPackages.rocminfo
       mangohud
