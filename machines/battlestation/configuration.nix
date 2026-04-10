1675109bf8fc51e0cfdd1bf84d38d4cbd4c2d8bd
Fredrik Bergstrom <basn@lan2k.org>
2026-04-10 08:20:12 +0200

    removed substituters since it does not compile v3 kernel

--- machines/battlestation/configuration.nix
+++ machines/battlestation/configuration.nix
@@ -339,9 +339,5 @@ in
   };
   nix = {
     package = unstableSmall.nix;
-    settings = {
-      substituters = [ "https://attic.xuyh0120.win/lantian" ];
-      trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-    };
   };
 }
