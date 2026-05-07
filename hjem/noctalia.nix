d29807e926bdd2a8a77c36004616a7044e399bb3
Fredrik Bergstrom <basn@lan2k.org>
2026-05-07 20:31:26 +0200

    test noctalia screenshot thingy

--- hjem/noctalia.nix
+++ hjem/noctalia.nix
@@ -1,10 +1,19 @@
 { pkgs, ... }:
 {
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
   hjem = {
     users = {
       basn = {
         packages = with pkgs; [
           noctalia-shell
+          satty
         ];
         files = {
           ".config/noctalia/settings.json".source = ./configs/noctalia-settings.json;
