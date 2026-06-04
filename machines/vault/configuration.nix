f0b7aff69e83dbe6f73a23b2a615ebf1ffbfb88c
Fredrik Bergstrom <basn@lan2k.org>
2026-06-04 12:24:23 +0200

    tried newer kernel on vault

--- machines/vault/configuration.nix
+++ machines/vault/configuration.nix
@@ -1,4 +1,17 @@
-{ inputs, pkgs, ... }:
+{ inputs, pkgs, lib, config, ... }:
+let
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
 {
   imports = [
     inputs.sops_nix.nixosModules.sops
@@ -12,6 +25,7 @@
   boot = {
     kernelModules = [ "kvm-intel" ];
     kernelParams = [ "pcie_aspm=off" ];
+    kernelPackages = latestKernelPackage;
     supportedFilesystems = [ "zfs" ];
     initrd = {
       kernelModules = [ ];
