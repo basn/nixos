d5a99ea4783cdbd8728bf0d1437f4664cfc5ca8e
Fredrik Bergstrom <basn@lan2k.org>
2026-07-14 12:09:03 +0200

    updated plex

--- machines/bandit/services/plex.nix
+++ machines/bandit/services/plex.nix
@@ -5,10 +5,10 @@
     let
       plexPass = pkgs.plex.override {
         plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
-          version = "1.43.3.10793-cd55560bb";
+          version = "1.43.3.10828-00f62d37d";
           src = pkgs.fetchurl {
             url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-            sha256 = "1skj4f59144bcv04h6bp4w3ajjw7bqrmsvw2wbbx9n6ml0gxppc9";
+            sha256 = "0vckm2ip610xhcp60zhcms4yjkyah8yq8l0xn6nwvbk5npzk9rc9";
           };
         });
       };
