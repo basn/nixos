ef92841455944533409898469a45fd10e3d7bfca
Fredrik Bergstrom <basn@lan2k.org>
2026-08-26 17:27:00 +0200

    bandit: update Plex to 1.43.4.10903

--- machines/bandit/services/plex.nix
+++ machines/bandit/services/plex.nix
@@ -5,10 +5,10 @@
     let
       plexPass = pkgs.plex.override {
         plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
-          version = "1.43.3.10896-cb3ebc72d";
+          version = "1.43.4.10903-e5521bd8c";
           src = pkgs.fetchurl {
             url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-            sha256 = "0jbiydwlrgb5d5mkj5llp84y3rjlwjv639bvrw88wh6gvmkg42da";
+            sha256 = "1nry7dffb5lv30mqmcb6d9pssjw8951r79ji07rf2yfp6s1iqskg";
           };
         });
       };
